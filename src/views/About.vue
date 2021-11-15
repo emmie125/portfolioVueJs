f9a3abbaa32ce4c84bc2ace904871c7ac68ccba2
emmie125 <mulangaemmanuella@gmail.com>
2021-11-15 09:33:57 +0100

    style: add a color and a paddind in the pages about

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,40 +3,60 @@
     <b-row  class="text-center">
     <Title tilte='À propros'/>
   </b-row>
-  <b-row class="flex-wrap ">
+  <b-row class="flex-wrap">
     <b-col  lg="5" sm="12">
-      <b-img class="img-fluid container__About__photo" center :src="EmmiePhoto" alt="Source code">
+      <b-img class="img-fluid container__About__photo m-2"
+      center :src="EmmiePhoto" alt="Source code">
     </b-img></b-col>
     <b-col  lg="7" sm="12">
-      <b-row  class="m-3">
+      <b-row  class="m-2">
          <b-col lg="2" sm="2">
            <Icon icon="gis:position" class="logoColor"/>
           </b-col>
-          <b-col>3 of 3</b-col>
+          <b-col><p>Kinshasa,RDCongo</p></b-col>
       </b-row>
-      <b-row  class="m-3">
+      <b-row  class="m-2">
         <b-col lg="2" sm="2">
           <Icon icon="map:university" class="logoColor"/>
         </b-col>
-        <b-col>3 of 3</b-col>
+        <b-col><p>Bac +5, Informatique</p></b-col>
       </b-row>
-      <b-row  class="m-3">
+      <b-row  class="m-2">
         <b-col lg="2" sm="2">
           <Icon icon="mdi:gmail" class="logoColor"/>
         </b-col>
-        <b-col>3 of 3</b-col>
+         <b-col>
+           <b-link href="mailto:mulangaemmanuella@gmail.com">mulangaemmanuella@gmail.com</b-link>
+        </b-col>
       </b-row>
-      <b-row  class="m-3">
+      <b-row  class="m-2">
         <b-col lg="2" sm="2">
           <Icon icon="akar-icons:github-fill" class="logoColor"/>
         </b-col>
-        <b-col>3 of 3</b-col>
+        <b-col><b-link href="https://github.com/emmie125/">emmie125</b-link></b-col>
       </b-row>
-      <b-row  class="m-3">
+      <b-row  class="m-2">
         <b-col lg="2" sm="2">
           <Icon icon="bx:bxs-phone" class="logoColor"/>
         </b-col>
-        <b-col>3 of 3</b-col>
+        <b-col><b-link href="tel:+243826370009">+243826370009</b-link></b-col>
+      </b-row>
+      <b-row  class="m-2">
+        <b-col class="justify-content-center">
+          <p>Je m'appelle Emmanuella, je suis née à Kinshasa et
+          j'y ai passé toute ma vie d’étudiante.
+          J'aime la découverte et la technologie
+          c'est pourquoi je suis diplômée en informatique depuis 2018 de l'université de Kinshasa.
+          Etant passionnée par ce métier, j'ai également entrepris une
+          formation dans le développement d'applications
+          web mobile dont je suis titulaire d'un certification Bac+2
+          en informatique de simplon à Kinshasa Digital.</p>
+        </b-col>
+      </b-row>
+      <b-row  class="m-2">
+        <b-col>
+          <Button name="project" text="Mes projets" color="red" />
+        </b-col>
       </b-row>
   </b-col>
   </b-row>
@@ -46,12 +66,14 @@
 import { Icon } from '@iconify/vue2';
 import Title from '../components/Title.vue';
 import EmmiePhoto from '../assets/emmieProfil.jpeg';
+import Button from '../components/ButtonPrimary.vue';
 
 export default {
   name: 'About',
   components: {
     Title,
     Icon,
+    Button,
   },
   data() {
     return {
@@ -68,6 +90,17 @@ export default {
     &__photo{
      border:5px solid $colorSecondary;
     }
+ }
+ p{
+     color: $colorPolice;
+     font-size:$textSize;
+ }
+ a{
+   color: $colorPolice;
+   font-size:$textSize;
+  &:hover{
+    color: $colorPolice;
+  }
  }
  .logoColor{
    @include iconStyle;
